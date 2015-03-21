2d1bf8769dfd88263e41890f96e9c85a9fbe95dc
MDTech-us-MAN <admin@mdtech.us>
2015-03-21 13:48:24 -0400

    Start Project

---
+++ SimpleInstaller/chkNET.cpp
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////
+///                                            ///
+///       Written by: Maxwell Dreytser         ///
+///          Email: admin@mdtech.us            /// 
+///  IRC: irc.msnirc.net Nick: MDTech-us_MAN   ///
+///             Channel: #MDTech               ///
+///                                            ///
+//////////////////////////////////////////////////
+
+
+#include <string>;
+
+using namespace std;
+using std::string;
+
+class chkDotNet {
+public:
+	// Begin Variables.
+	string DotNetUri;
+	// End Variables.
+
+	// Begin Con-/De-struter
+	chkDotNet() {
+		DotNetUri = "http://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe";
+	}
+	// End Con-/De-struter
+
+	// Begin Functions
+
+	// End Functions
+};
